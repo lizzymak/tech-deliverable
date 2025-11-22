75779b82200e30156b0d0568333e12867ec8dc82
lizzymak <146508325+lizzymak@users.noreply.github.com>
2025-11-21 23:23:55 -0800

    Style for page is done, added logo asset, and added logo img to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import QuoteForm from "./components/QuoteForm";
 import QuoteList from "./components/QuoteList";
+import logo from "./assets/quotebook.png";
 
 const API_BASE = "http://localhost:8000" //port out backend runs on
 
@@ -10,23 +11,36 @@ function App() {
   	const [refreshKey, setRefreshKey] = useState(0); // to trigger QuoteList refresh
 	return (
 		<div className="App">
-			{/* TODO: include an icon for the quote book */}
-			<h1>Hack at UCI Tech Deliverable</h1>
+			<div className="top-header">
+				<img src={logo} alt="Hack Logo" />
+				<h1>Hack at UCI Tech Deliverable</h1>
+			</div>
+			
 
-			<h2>Submit a quote</h2>
-      		<QuoteForm apiBaseUrl={API_BASE} onQuoteSubmitted={() => setRefreshKey((prev) => prev + 1)}/>
+			<div className="main-area">
+				<div className="form-container">
+					<h2>Submit a quote</h2>
+      				<QuoteForm apiBaseUrl={API_BASE} onQuoteSubmitted={() => setRefreshKey((prev) => prev + 1)}/>
+				</div>
+				
 
-			<h2>Previous Quotes</h2>
-			<label>Show quotes from:
-        	<select value={maxAge} onChange={(e) => setMaxAge(Number(e.target.value))}>
-          		<option value={7 * 24 * 3600}>Last week</option>
-          		<option value={30 * 24 * 3600}>Last month</option>
-          		<option value={365 * 24 * 3600}>Last year</option>
-          		<option value={0}>All time</option>
-        	</select>
-      		</label>
+				<div className="max-age-container">
+					<h2>Previous Quotes</h2>
+					<label>Show quotes from:
+					<select value={maxAge} onChange={(e) => setMaxAge(Number(e.target.value))}>
+						<option value={7 * 24 * 3600}>Last week</option>
+						<option value={30 * 24 * 3600}>Last month</option>
+						<option value={365 * 24 * 3600}>Last year</option>
+						<option value={0}>All time</option>
+					</select>
+					</label>
+				</div>
+				
+		
+				<QuoteList apiBaseUrl={API_BASE} maxAge={maxAge} key={refreshKey} />
 
-			<QuoteList apiBaseUrl={API_BASE} maxAge={maxAge} key={refreshKey} />
+			</div>
+			
 			
 		</div>
 	)
