3cdac21324ea1b4c7f2381b64871b46721c20bc4
lizzymak <146508325+lizzymak@users.noreply.github.com>
2025-11-21 21:09:30 -0800

    Added GET /quotes endpoint with max_age query parameter
    - Returns quotes newer than max_age seconds
    - Returns all quotes if max_age is 0

--- api/src/app.py
+++ api/src/app.py
@@ -8,6 +8,8 @@ from typing_extensions import TypedDict
 
 from services.database import JSONDatabase
 
+from datetime import datetime, timedelta
+
 
 class Quote(TypedDict):
     name: str
@@ -48,3 +50,26 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
 
 
 # TODO: add another API route with a query parameter to retrieve quotes based on max age
+@app.get("/quotes")
+def get_quotes(max_age: int = 0):
+    """
+    Get quotes newer than max_age seconds.
+    If max_age = 0, return all quotes.
+    """
+    if max_age == 0:
+        return database["quotes"]
+    
+    now = datetime.now()
+
+    """
+    Iterate through each quote in the database
+    Convert each quote's time that is a string into a python date time object
+    Subtract that from the current time to obtain the amount of time that has passed since a quote has been posted
+    Comparison operator checks if the calculated time is less than or equal to the max_age duration
+    We then return a list of all the quotes that were posted within the max_age
+    """
+
+    return [
+        quote for quote in database["quotes"]
+        if now - datetime.fromisoformat(quote["time"]) <= timedelta(seconds=max_age)
+    ]
